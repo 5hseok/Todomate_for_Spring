650319f0681aced92c146e9d609e7b7ff589607c
5hseok <ohhs1010@gmail.com>
2024-03-06 11:30:38 +0900

    Feat : Book Domain 객체 Book 객체를 @Entity로 묶어 JPA를 사용

---
+++ src/main/java/com/example/Todomate/domain/Book/Book.java
@@ -0,0 +1,27 @@
+package com.example.Todomate.domain.Book;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Primary;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    protected Book() {
+    }
+
+    public Book(String name) {
+        if(name == null || name.isEmpty()){
+            throw new IllegalArgumentException("잘못된 책 이름이 입력되었습니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
