f518f735d13137365bb9b70d2502660e21f29187
5hseok <ohhs1010@gmail.com>
2024-03-19 14:44:40 +0900

    Feat : findByName JPA 메소드 추가

--- src/main/java/com/example/Todomate/repository/Book/BookRepository.java
+++ src/main/java/com/example/Todomate/repository/Book/BookRepository.java
@@ -3,6 +3,9 @@ package com.example.Todomate.repository.Book;
 import com.example.Todomate.domain.Book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByName(String name);
 }
