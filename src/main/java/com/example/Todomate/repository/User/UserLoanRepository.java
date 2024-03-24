9d047497b639ac0cd30b285d6bb49bf2be7fb958
5hseok <ohhs1010@gmail.com>
2024-03-24 20:23:09 +0900

    Feat : UserLoanRepository interface 구현 existByNameAndIsReturn 메소드 정의

---
+++ src/main/java/com/example/Todomate/repository/User/UserLoanRepository.java
@@ -0,0 +1,9 @@
+package com.example.Todomate.repository.User;
+
+import com.example.Todomate.domain.User.UserLoanHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserLoanRepository extends JpaRepository<UserLoanHistory, Long> {
+    boolean existsByNameAndIsReturn(String bookName, boolean isReturn);
+}
