37c3013bec85c8030d01862f2cc6ff8df2d5b3c7
5hseok <ohhs1010@gmail.com>
2024-03-26 14:59:00 +0900

    Feat : 대출기록 Entity 객체 생성 대출 기록 Entity를 새로 만든 이유에 대해 자세히 알아보기

---
+++ src/main/java/com/example/Todomate/domain/User/UserLoanHistory.java
@@ -0,0 +1,22 @@
+package com.example.Todomate.domain.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserLoanHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+    private long userid;
+    private String bookName;
+    private boolean loan;
+
+    public UserLoanHistory(long userid, String bookName) {
+        this.userid = userid;
+        this.bookName = bookName;
+        this.loan = false;
+    }
+}
