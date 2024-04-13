37b96f230cb6961b422434218c44b0d5f10e629c
5hseok <ohhs1010@gmail.com>
2024-04-14 00:50:54 +0900

    :fire: Optional 패키지 삭제 개발 과정에서 사용했던 Optional 코드 삭제로 인한 import 문 삭제

--- src/main/java/com/example/Todomate/repository/User/UserLoanRepository.java
+++ src/main/java/com/example/Todomate/repository/User/UserLoanRepository.java
@@ -2,7 +2,6 @@ package com.example.Todomate.repository.User;
 
 import com.example.Todomate.domain.User.UserLoanHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UserLoanRepository extends JpaRepository<UserLoanHistory, Long> {
     boolean existsByBookNameAndLoan(String bookName, boolean loan);
